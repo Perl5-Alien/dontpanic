6e7e4be9b61256a9364306068099e07862c3117b
Graham Ollis <plicease@cpan.org>
2016-06-22 11:51:50 -0400

    improve autoclean

--- autoclean.sh
+++ autoclean.sh
@@ -1,5 +1,19 @@
-make clean;
-rm aclocal.m4 config.* configure Makefile Makefile.in stamp-h1 src/Makefile src/Makefile.in;
+#!/bin/bash -x
+
+set -euo pipefail
+IFS=$'\n\t'
+
+if [ -f configure ]; then
+  if ! [ -f Makefile ]; then
+    ./configure
+  fi
+fi
+
+if [ -f Makefile ]; then
+  make clean;
+fi
+
+rm -f aclocal.m4 config.* configure Makefile Makefile.in stamp-h1 src/Makefile src/Makefile.in;
 rm -Rf autom4te.cache libtool;
-rm -Rf m4/* config/* src/.deps;
+rm -Rf m4 config src/.deps src/dontpanic.pc;
 
